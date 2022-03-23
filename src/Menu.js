60229ce3161b8034d6647222fd52a47ac2aa34a6
Joe Martella <joem@microsoft.com>
2022-03-23 12:05:42 -0700

    bump

--- src/Menu.js
+++ src/Menu.js
@@ -46,7 +46,7 @@ export default function Menu(props) {
           }}
         >
           <div>#{puzzleNumber}</div>
-          <div>v0.0.10</div>
+          <div>v0.0.11</div>
         </div>
       </div>
     </>
