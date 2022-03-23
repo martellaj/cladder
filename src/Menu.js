6bbffdba34a2212f450806fbcc7cae04b2d270c4
Joe Martella <joem@microsoft.com>
2022-03-23 15:40:52 -0700

    twitter analytics

--- src/Menu.js
+++ src/Menu.js
@@ -46,7 +46,7 @@ export default function Menu(props) {
           }}
         >
           <div>#{puzzleNumber}</div>
-          <div>v0.0.14</div>
+          <div>v0.0.15</div>
         </div>
       </div>
     </>
