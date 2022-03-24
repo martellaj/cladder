ee52e6d7853a86fa3a7988c270e5090a8f73a2b0
Joe Martella <joem@microsoft.com>
2022-03-24 09:19:26 -0700

    changes icon

--- src/Menu.js
+++ src/Menu.js
@@ -46,7 +46,7 @@ export default function Menu(props) {
           }}
         >
           <div>#{puzzleNumber}</div>
-          <div>v0.0.17</div>
+          <div>v0.0.18</div>
         </div>
       </div>
     </>
