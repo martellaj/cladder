aea9bfb1236591de6892f015404b4f23979cde5d
Joe Martella <martellaj@outlook.com>
2022-03-29 20:57:55 -0700

    how

--- src/HowToPlay.js
+++ src/HowToPlay.js
@@ -4,15 +4,10 @@ export default function HowToPlay() {
       <h3 style={{ marginBottom: "30px" }}>HOW TO PLAY</h3>
       <div>
         <h4>🕵️‍♂️ You have 60 seconds to solve all 10 clues</h4>
-        <h4>
-          1️⃣ Each answer is only 1 letter different from the previous answer
-          (e.g. JOE ➡️ JOT)
-        </h4>
-        <h4>
-          🙅‍♀️ You are <strong>not</strong> penalized for incorrect guesses
-        </h4>
+        <h4>1️⃣ Change 1 letter from the previous word to get the answer</h4>
+        <h4>🙅‍♀️ You are not penalized for incorrect guesses</h4>
         <h4>⏭ You have 2 skips (each skip costs 5 seconds)</h4>
-        <h4>🕛 New puzzle at midnight!</h4>
+        <h4>🕛 New puzzle at midnight</h4>
       </div>
     </div>
   );
