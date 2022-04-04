694483120771bd8921876b6133b773c52c411027
Joe Martella <martellaj@outlook.com>
2022-04-03 20:59:25 -0700

    shortens

--- src/Game.js
+++ src/Game.js
@@ -429,7 +429,7 @@ export default function Game(props) {
               ],
             }}
             display={{
-              "{backspace}": " backspace ",
+              "{backspace}": " delete ",
             }}
           />
         </div>
