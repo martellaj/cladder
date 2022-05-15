4a4d58a8b904e72e2c615d72979a48d72d20ec58
Joe Martella <martellaj@outlook.com>
2022-05-15 15:55:19 -0700

    adds a nice bounce

--- src/Tile.js
+++ src/Tile.js
@@ -1,4 +1,5 @@
 import "./Tile.css";
+import { /*useEffect, */ useState } from "react";
 
 function Tile(props) {
   const {
@@ -12,14 +13,36 @@ function Tile(props) {
     completed = false,
   } = props;
 
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  // useEffect(() => {
+  //   if (letter && !isSelected) {
+  //     setIsAnimating(true);
+
+  //     setTimeout(() => {
+  //       setIsAnimating(false);
+  //     }, 500);
+  //   }
+  // }, [letter, isSelected]);
+
   return (
     <div
-      onClick={onSelected}
+      onClick={() => {
+        if (!isSelected) {
+          setIsAnimating(true);
+
+          setTimeout(() => {
+            setIsAnimating(false);
+          }, 500);
+        }
+
+        onSelected();
+      }}
       className={`tile ${isSelected && "selected"} ${altered && "altered"} ${
         attempted && "attempted"
       } ${completed && "completed"} ${failed && "failed"} ${
         wordLength === 5 && "fiveLetterTile"
-      }`}
+      } ${isAnimating && "animateTile"}`}
     >
       {letter}
     </div>
