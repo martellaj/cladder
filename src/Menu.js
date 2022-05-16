92d8419b572668a8d26e163608f43bf6e1801f1f
Joe Martella <martellaj@outlook.com>
2022-05-15 20:13:15 -0700

    changes button

--- src/Menu.js
+++ src/Menu.js
@@ -17,91 +17,87 @@ export default function Menu(props) {
   }, []);
 
   return (
-    <>
-      <div className="menuContainer">
-        <div className="menuButton">
-          <Button
-            id="playButton"
-            size="massive"
-            className="menuButton button"
-            color="green"
-            inverted={isDarkMode}
-            onClick={() => onOptionSelected("game")}
-          >
-            PLAY
-          </Button>
-        </div>
-        {showChallengeMode && (
-          <div className="menuButton">
-            <Button
-              id="challengeButton"
-              size="massive"
-              className="menuButton button"
-              color="yellow"
-              inverted={isDarkMode}
-              onClick={() => onOptionSelected("challenge")}
-            >
-              CHALLENGE
-            </Button>
-          </div>
-        )}
-        <div className="menuButton">
-          <Button
-            id="archiveButton"
-            size="massive"
-            className="menuButton button"
-            color="orange"
-            inverted={isDarkMode}
-            onClick={() => onOptionSelected("archive")}
-          >
-            ARCHIVE
-          </Button>
-        </div>
-        <div className="menuButton">
-          <Button
-            id="practiceButton"
-            size="massive"
-            className="menuButton button"
-            color="red"
-            inverted={isDarkMode}
-            onClick={() => onOptionSelected("practice")}
-          >
-            PRACTICE
-          </Button>
-        </div>
-        {!showChallengeMode && (
-          <div className="menuButton">
-            <Button
-              size="massive"
-              className="menuButton button"
-              color="yellow"
-              inverted={isDarkMode}
-              onClick={() => onOptionSelected("howToPlay")}
-            >
-              HOW TO PLAY
-            </Button>
-          </div>
-        )}
-        <div className="menuButton">
-          <Button
-            size="massive"
-            className="menuButton button"
-            onClick={() => onOptionSelected("about")}
-            inverted={isDarkMode}
-          >
-            ABOUT
-          </Button>
-        </div>
-        <div
-          style={{
-            marginBottom: "24px",
-            marginTop: "auto",
-          }}
+    <div className="menuContainer">
+      <div className="menuButton">
+        <Button
+          id="playButton"
+          size="massive"
+          className="menuButton button"
+          color="green"
+          inverted={isDarkMode}
+          onClick={() => onOptionSelected("game")}
         >
-          <div>#{puzzleNumber}</div>
-          <div>v1.1.0</div>
-        </div>
+          PLAY
+        </Button>
       </div>
-    </>
+      {showChallengeMode && (
+        <div className="menuButton">
+          <Button
+            id="challengeButton"
+            size="massive"
+            className="menuButton button"
+            color="yellow"
+            inverted={isDarkMode}
+            onClick={() => {
+              window.localStorage.setItem("challengePageSeen", "true");
+              onOptionSelected("challenge");
+            }}
+          >
+            CHALLENGE
+          </Button>
+        </div>
+      )}
+      <div className="menuButton">
+        <Button
+          id="archiveButton"
+          size="massive"
+          className="menuButton button"
+          color="orange"
+          inverted={isDarkMode}
+          onClick={() => onOptionSelected("archive")}
+        >
+          ARCHIVE
+        </Button>
+      </div>
+      <div className="menuButton">
+        <Button
+          id="practiceButton"
+          size="massive"
+          className="menuButton button"
+          color="red"
+          inverted={isDarkMode}
+          onClick={() => onOptionSelected("practice")}
+        >
+          PRACTICE
+        </Button>
+      </div>
+      {!showChallengeMode && (
+        <div className="menuButton">
+          <Button
+            size="massive"
+            className="menuButton button"
+            color="yellow"
+            inverted={isDarkMode}
+            onClick={() => onOptionSelected("howToPlay")}
+          >
+            HOW TO PLAY
+          </Button>
+        </div>
+      )}
+      <div
+        style={{
+          marginBottom: "24px",
+          marginTop: "auto",
+          fontVariant: "all-small-caps",
+        }}
+      >
+        <div>#{puzzleNumber}</div>
+        <div>v1.1.0</div>
+        {/* eslint-disable-next-line */}
+        <a id="contact" onClick={() => onOptionSelected("about")}>
+          CONTACT
+        </a>
+      </div>
+    </div>
   );
 }
