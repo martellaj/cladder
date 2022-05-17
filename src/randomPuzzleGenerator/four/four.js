3eb26d76987f34ea8cb284dbae06b6d1eb18b954
Joe Martella <joem@microsoft.com>
2022-05-17 16:12:19 -0700

    help

--- src/randomPuzzleGenerator/four/four.js
+++ src/randomPuzzleGenerator/four/four.js
@@ -8,81 +8,85 @@ import four from "./fourData";
 const fourKeys = Object.keys(four);
 
 const getRandomFourLetterPuzzle = () => {
-  const randomizer = getRandomizer();
-
-  const initialWord = fourKeys[randomizer];
-  const nextLevel = getNextLevel(initialWord);
-
-  const puzzle = [
-    {
-      word: initialWord,
-      hint: nextLevel.hint,
-      answer: nextLevel.answer,
-      alteredPosition: getAlteredPosition(initialWord, nextLevel.answer),
-    },
-  ];
-  const puzzleAnswers = [initialWord, nextLevel.answer];
-
-  while (puzzle.length < 10) {
-    const previousAnswer = puzzle[puzzle.length - 1].answer;
-    const nextNextLevel = getNextLevel(
-      previousAnswer,
-      getRestrictedPosition(puzzle)
-    );
-
-    if (!puzzleAnswers.includes(nextNextLevel.answer)) {
-      puzzle.push({
-        word: previousAnswer,
-        hint: nextNextLevel.hint,
-        answer: nextNextLevel.answer,
-        alteredPosition: getAlteredPosition(
-          previousAnswer,
-          nextNextLevel.answer
-        ),
-      });
-
-      puzzleAnswers.push(nextNextLevel.answer);
-    }
-  }
-
-  return puzzle;
-};
-
-const getNextLevel = (word, restrictedPosition = -1) => {
   try {
     const randomizer = getRandomizer();
 
-    let attempts = 0;
+    const initialWord = fourKeys[randomizer];
+    const nextLevel = getNextLevel(initialWord);
 
-    for (let i = randomizer; i < fourKeys.length; i++) {
-      const candidateWord = fourKeys[i];
+    const puzzle = [
+      {
+        word: initialWord,
+        hint: nextLevel.hint,
+        answer: nextLevel.answer,
+        alteredPosition: getAlteredPosition(initialWord, nextLevel.answer),
+      },
+    ];
+    const puzzleAnswers = [initialWord, nextLevel.answer];
 
-      if (
-        /^[a-zA-Z]+$/.test(candidateWord) === true &&
-        isValidNextLevel(word, candidateWord, restrictedPosition)
-      ) {
-        const hints = four[candidateWord];
-        const randomHint = hints[Math.floor(Math.random() * hints.length)];
+    while (puzzle.length < 10) {
+      const previousAnswer = puzzle[puzzle.length - 1].answer;
+      const nextNextLevel = getNextLevel(
+        previousAnswer,
+        getRestrictedPosition(puzzle)
+      );
 
-        return {
-          answer: candidateWord,
-          hint: randomHint,
-        };
+      if (!nextNextLevel) {
+        return getRandomFourLetterPuzzle();
       }
 
-      // reset if we haven't found a next word
-      if (i === fourKeys.length - 1) {
-        if (attempts === 1) {
-          window.location.href = "/";
-          return;
-        }
+      if (!puzzleAnswers.includes(nextNextLevel.answer)) {
+        puzzle.push({
+          word: previousAnswer,
+          hint: nextNextLevel.hint,
+          answer: nextNextLevel.answer,
+          alteredPosition: getAlteredPosition(
+            previousAnswer,
+            nextNextLevel.answer
+          ),
+        });
 
-        i = 0;
-        attempts++;
+        puzzleAnswers.push(nextNextLevel.answer);
       }
     }
+
+    return puzzle;
   } catch (e) {
-    window.location.href = "/";
+    console.log("failed to get a puzzle");
+    return getRandomFourLetterPuzzle();
+  }
+};
+
+const getNextLevel = (word, restrictedPosition = -1) => {
+  const randomizer = getRandomizer();
+
+  let attempts = 0;
+
+  for (let i = randomizer; i < fourKeys.length; i++) {
+    const candidateWord = fourKeys[i];
+
+    if (
+      /^[a-zA-Z]+$/.test(candidateWord) === true &&
+      isValidNextLevel(word, candidateWord, restrictedPosition)
+    ) {
+      const hints = four[candidateWord];
+      const randomHint = hints[Math.floor(Math.random() * hints.length)];
+
+      return {
+        answer: candidateWord,
+        hint: randomHint,
+      };
+    }
+
+    // reset if we haven't found a next word
+    if (i === fourKeys.length - 1) {
+      if (attempts === 1) {
+        return null;
+      }
+
+      i = 0;
+      attempts++;
+    }
   }
 };
 
