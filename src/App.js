681dcb89be520edf085169a186a05452b1026e2b
Joe Martella <joem@microsoft.com>
2022-07-26 14:33:22 -0700

    b

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ function App() {
 
   // switch over to true when app launches
   // switched over on 7/26
-  const shouldPromote = params?.app || true;
+  const shouldPromote = params?.app || false;
 
   useEffect(() => {
     if (params?.convert) {
